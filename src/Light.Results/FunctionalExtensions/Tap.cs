852b64f596992639db1ace0198c298be89142d58
Kenny Pflug <kenny.pflug@live.de>
2026-01-14 22:52:52 +0100

    feat: add functional extensions
    Signed-off-by: Kenny Pflug <kenny.pflug@live.de>

---
+++ src/Light.Results/FunctionalExtensions/Tap.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Threading.Tasks;
+
+// ReSharper disable ConvertToExtensionBlock
+
+namespace Light.Results.FunctionalExtensions;
+
+/// <summary>
+/// Provides Tap extension methods for result types.
+/// </summary>
+public static class TapExtensions
+{
+    /// <summary>
+    /// Executes the specified action on the value if this result is valid.
+    /// </summary>
+    /// <typeparam name="T">The value type.</typeparam>
+    /// <param name="result">The result to tap.</param>
+    /// <param name="action">The action to execute on success.</param>
+    /// <returns>The original result.</returns>
+    public static Result<T> Tap<T>(this Result<T> result, Action<T> action)
+    {
+        if (result.IsValid)
+        {
+            action(result.Value);
+        }
+
+        return result;
+    }
+
+    /// <summary>
+    /// Executes the specified async action on the value if this result is valid.
+    /// </summary>
+    /// <typeparam name="T">The value type.</typeparam>
+    /// <param name="result">The result to tap.</param>
+    /// <param name="action">The async action to execute on success.</param>
+    /// <returns>A task containing the original result.</returns>
+    public static async ValueTask<Result<T>> TapAsync<T>(
+        this Result<T> result,
+        Func<T, ValueTask> action
+    )
+    {
+        if (result.IsValid)
+        {
+            await action(result.Value).ConfigureAwait(false);
+        }
+
+        return result;
+    }
+}
