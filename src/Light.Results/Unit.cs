d949ec07d293a6f849dd6e862de14f655abc8738
Kenny Pflug <kenny.pflug@live.de>
2026-01-14 13:49:29 +0100

    chore: fix XML comment in Unit.cs
    Signed-off-by: Kenny Pflug <kenny.pflug@live.de>

--- src/Light.Results/Unit.cs
+++ src/Light.Results/Unit.cs
@@ -7,8 +7,9 @@ namespace Light.Results;
 /// <para>
 /// In functional programming, Unit is often used as a placeholder in functions that do not return a value,
 /// but still want to use the Result Pattern. This library has the concept of Unit, but allows to use
-/// <see cref="Result"/> instead of <c>Result&lt;Unit></c>. Choose whichever you like best
-/// </para>. <see cref="Result"/> actually encapsulates a <c>Result&lt;Unit></c> instance.
+/// <see cref="Result"/> instead of <c>Result&lt;Unit></c>. Choose whichever you like best.
+/// <see cref="Result"/> actually encapsulates a <c>Result&lt;Unit></c> instance.
+/// </para>.
 /// </summary>
 public readonly record struct Unit
 {
