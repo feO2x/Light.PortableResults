fdc277a49dc726378eb33d435921f94e06a3019c
Kenny Pflug <kenny.pflug@live.de>
2026-04-30 15:30:42 +0200

    chore: introduce Swashbuckle UI in NativeAotMovieRating.csproj
    Signed-off-by: Kenny Pflug <kenny.pflug@live.de>

--- samples/NativeAotMovieRating/OpenApi/OpenApiModule.cs
+++ samples/NativeAotMovieRating/OpenApi/OpenApiModule.cs
@@ -10,6 +10,12 @@ public static class OpenApiModule
     {
         app.MapOpenApi();
         app.MapScalarApiReference("/docs", options => options.WithTitle("Native AOT Movie Rating API"));
+        app.UseSwaggerUI(options =>
+        {
+            options.DocumentTitle = "Native AOT Movie Rating API - Swagger UI";
+            options.RoutePrefix = "swagger";
+            options.SwaggerEndpoint("/openapi/v1.json", "Native AOT Movie Rating API v1");
+        });
     }
 
     public static void RedirectHomeToDocs(this WebApplication app) =>
